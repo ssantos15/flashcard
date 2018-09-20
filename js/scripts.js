17e6624d1157e1ef69f82b5180c838f3035c76a6
Steve Santos <padma@email.com>
2018-09-20 12:16:14 -0700

    This webpage will have programming terms to study in the form of notecards using event handlers to fade in and out.

---
+++ js/scripts.js
@@ -0,0 +1,154 @@
+$(document).ready(function() {
+  $("#oneH").click(function() {
+    $("#oneH").fadeOut();
+    $("#oneP").fadeIn();
+  });
+  $("#oneP").click(function() {
+    $("#oneP").fadeOut();
+    $("#oneH").fadeIn();
+  });
+  $("#twoH").click(function() {
+    $("#twoH").fadeOut();
+    $("#twoP").fadeIn();
+  });
+  $("#twoP").click(function() {
+    $("#twoP").fadeOut();
+    $("#twoH").fadeIn();
+  });
+  $("#threeH").click(function() {
+    $("#threeH").fadeOut();
+    $("#threeP").fadeIn();
+  });
+  $("#threeP").click(function() {
+    $("#threeP").fadeOut();
+    $("#threeH").fadeIn();
+  });
+  $("#fourH").click(function() {
+    $("#fourH").fadeOut();
+    $("#fourP").fadeIn();
+  });
+  $("#fourP").click(function() {
+    $("#fourP").fadeOut();
+    $("#fourH").fadeIn();
+  });
+  $("#fiveH").click(function() {
+    $("#fiveH").fadeOut();
+    $("#fiveP").fadeIn();
+  });
+  $("#fiveP").click(function() {
+    $("#fiveP").fadeOut();
+    $("#fiveH").fadeIn();
+  });
+  $("#sixH").click(function() {
+    $("#sixH").fadeOut();
+    $("#sixP").fadeIn();
+  });
+  $("#sixP").click(function() {
+    $("#sixP").fadeOut();
+    $("#sixH").fadeIn();
+  });
+  $("#sevenH").click(function() {
+    $("#sevenH").fadeOut();
+    $("#sevenP").fadeIn();
+  });
+  $("#sevenP").click(function() {
+    $("#sevenP").fadeOut();
+    $("#sevenH").fadeIn();
+  });
+  $("#eightH").click(function() {
+    $("#eightH").fadeOut();
+    $("#eightP").fadeIn();
+  });
+  $("#eightP").click(function() {
+    $("#eightP").fadeOut();
+    $("#eightH").fadeIn();
+  });
+  $("#nineH").click(function() {
+    $("#nineH").fadeOut();
+    $("#nineP").fadeIn();
+  });
+  $("#nineP").click(function() {
+    $("#nineP").fadeOut();
+    $("#nineH").fadeIn();
+  });
+  $("#tenH").click(function() {
+    $("#tenH").fadeOut();
+    $("#tenP").fadeIn();
+  });
+  $("#tenP").click(function() {
+    $("#tenP").fadeOut();
+    $("#tenH").fadeIn();
+  });
+  $("#elevenH").click(function() {
+    $("#elevenH").fadeOut();
+    $("#elevenP").fadeIn();
+  });
+  $("#elevenP").click(function() {
+    $("#elevenP").fadeOut();
+    $("#elevenH").fadeIn();
+  });
+  $("#twelveH").click(function() {
+    $("#twelveH").fadeOut();
+    $("#twelveP").fadeIn();
+  });
+  $("#twelveP").click(function() {
+    $("#twelveP").fadeOut();
+    $("#twelveH").fadeIn();
+  });
+  $("#thirteenH").click(function() {
+    $("#thirteenH").fadeOut();
+    $("#thirteenP").fadeIn();
+  });
+  $("#thirteenP").click(function() {
+    $("#thirteenP").fadeOut();
+    $("#thirteenH").fadeIn();
+  });
+  $("#fourteenH").click(function() {
+    $("#fourteenH").fadeOut();
+    $("#fourteenP").fadeIn();
+  });
+  $("#fourteenP").click(function() {
+    $("#fourteenP").fadeOut();
+    $("#fourteenH").fadeIn();
+  });
+  $("#fiveteenH").click(function() {
+    $("#fiveteenH").fadeOut();
+    $("#fiveteenP").fadeIn();
+  });
+  $("#fiveteenP").click(function() {
+    $("#fiveteenP").fadeOut();
+    $("#fiveteenH").fadeIn();
+  });
+  $("#sixteenH").click(function() {
+    $("#sixteenH").fadeOut();
+    $("#sixteenP").fadeIn();
+  });
+  $("#sixteenP").click(function() {
+    $("#sixteenP").fadeOut();
+    $("#sixteenH").fadeIn();
+  });
+  $("#seventeenH").click(function() {
+    $("#seventeenH").fadeOut();
+    $("#seventeenP").fadeIn();
+  });
+  $("#seventeenP").click(function() {
+    $("#seventeenP").fadeOut();
+    $("#seventeenH").fadeIn();
+  });
+  $("#eighteenH").click(function() {
+    $("#eighteenH").fadeOut();
+    $("#eighteenP").fadeIn();
+  });
+  $("#eighteenP").click(function() {
+    $("#eighteenP").fadeOut();
+    $("#eighteenH").fadeIn();
+  });
+  $("#nineteenH").click(function() {
+    $("#nineteenH").fadeOut();
+    $("#nineteenP").fadeIn();
+  });
+  $("#nineteenP").click(function() {
+    $("#nineteenP").fadeOut();
+    $("#nineteenH").fadeIn();
+  });
+});
